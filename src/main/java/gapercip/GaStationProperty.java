0d5a253d5b35c0db233842d314c62421522f8544
Karl Nicholas <Karl.Nicholas@outlook.com>
2021-11-28 18:42:19 -0500

    Refactoring

--- src/main/java/gapercip/GaStationProperty.java
+++ src/main/java/gapercip/GaStationProperty.java
@@ -1,15 +1,41 @@
 package gapercip;
 
+import com.fasterxml.jackson.databind.util.BeanUtil;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.apache.commons.csv.CSVRecord;
 
+import java.beans.BeanDescriptor;
+import java.beans.BeanInfo;
+import java.beans.BeanProperty;
 import java.math.BigDecimal;
 import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.Arrays;
+import java.util.Map;
 
 @Data
 @AllArgsConstructor
 public class GaStationProperty {
+    public enum GaStationPropertyHeader {
+        city,county,zipCode,localSiteName,latitude,longitude,elevation,siteId,dateOfInstallation,siteKey
+    }
+    public GaStationProperty(Map<String, String> map) {
+        this(
+                map.get("city"),
+                map.get("county"),
+                map.get("zipCode"),
+                map.get("localSiteName"),
+                new BigDecimal(map.get("latitude")),
+                new BigDecimal(map.get("longitude")),
+                new BigDecimal(map.get("elevation")),
+                map.get("siteId"),
+                LocalDate.parse(map.get("dateOfInstallation"), DateTimeFormatter.ofPattern("MM/dd/yyyy")),
+                map.get("siteKey")
+        );
+    }
+
     private String city;
     private String county;
     private String zipCode;
@@ -20,4 +46,5 @@ public class GaStationProperty {
     private String siteId;
     private LocalDate dateOfInstallation;
     private String siteKey;
+
 }
--- src/main/java/gapercip/GaWeatherApp.java
+++ src/main/java/gapercip/GaWeatherApp.java
@@ -22,6 +22,7 @@ import java.util.stream.Collectors;
 
 public class GaWeatherApp {
     private ObjectMapper mapper;
+
     public static void main(String[] args) throws IOException {
         new GaWeatherApp().run();
     }
@@ -30,35 +31,21 @@ public class GaWeatherApp {
         mapper = new ObjectMapper().registerModule(new JavaTimeModule());
         GaStationProperties gaStationProperties = loadStationProperties();
         GaStationReadings gaStationReadings = getGaStationReadings(gaStationProperties);
-        for ( GaStationReading gaStationReading: gaStationReadings.getGaStationReadings()) {
+        for (GaStationReading gaStationReading : gaStationReadings.getGaStationReadings()) {
             System.out.println(gaStationReading);
         }
     }
 
     private GaStationProperties loadStationProperties() throws IOException {
         GaStationProperties gaStationProperties = new GaStationProperties();
-        gaStationProperties.setGaStationProperties(new ArrayList<>());
-        try (InputStreamReader in = new InputStreamReader(GaWeatherApp.class.getResourceAsStream("/GaStationProperties.csv")) ) {
-            Iterable<CSVRecord> records = CSVFormat.RFC4180.parse(in);
-            int i=0;
-            for (CSVRecord record : records) {
-                GaStationProperty gaStationProperty = new GaStationProperty(
-                        record.get(0),
-                        record.get(1),
-                        record.get(2),
-                        record.get(3),
-                        new BigDecimal(record.get(4)),
-                        new BigDecimal(record.get(5)),
-                        new BigDecimal(record.get(6)),
-                        record.get(7),
-                        LocalDate.parse(record.get(8), DateTimeFormatter.ofPattern("MM/dd/yyyy")),
-                        record.get(9)
-                );
-                gaStationProperties.getGaStationProperties().add(gaStationProperty);
-            }
+        try (InputStreamReader in = new InputStreamReader(GaWeatherApp.class.getResourceAsStream("/GaStationProperties.csv"))) {
+            gaStationProperties.setGaStationProperties( CSVFormat.Builder.create()
+                    .setHeader(GaStationProperty.GaStationPropertyHeader.class)
+                    .build().parse(in).stream().map(CSVRecord::toMap).map(GaStationProperty::new).collect(Collectors.toList()));
         }
         return gaStationProperties;
     }
+
     private GaStationReadings getGaStationReadings(GaStationProperties gaStationProperties) {
         GaStationReadings gaStationReadings = new GaStationReadings();
         gaStationReadings.setGaStationReadings(new ArrayList<GaStationReading>());
@@ -79,44 +66,23 @@ public class GaWeatherApp {
         executor.shutdown();
         return gaStationReadings;
     }
+
     private GaStationReading getGaStationReading(String siteKey) throws IOException {
         Map<String, String> values = new HashMap<>();
+        values.put("siteKey", siteKey);
         Element body = Jsoup.parse(URI.create("http://weather.uga.edu/index.php?variable=CC&site=" + siteKey).toURL(), 10000).body();
         String observationDate = body.select("tr.TableTitleRow").select("td").get(0).text();
         values.put("observationDate", observationDate);
-        for ( Element element: body.select("tr.TableRow2[align=left]")) {
+        for (Element element : body.select("tr.TableRow2[align=left]")) {
             String tdName = element.select("td.tdClass").get(0).text().replace("&nbsp", "");
             Element td = element.select("td.tdClass").get(1);
-            if ( td.text().contentEquals("'") ) {
+            if (td.text().contentEquals("'")) {
                 values.put(tdName, td.select("img").attr("src").replace("/images/", "").replace("CUR.png", ""));
             } else {
-                values.put(tdName, td.text().replace("&nbsp", "")
-                        .replace("&degF", ""));
+                values.put(tdName, td.text().replace("&nbsp", "").replace("&degF", "F"));
             }
         }
-        GaStationReading gaStationReading = new GaStationReading(
-                siteKey,
-                values.get("observationDate"),
-                values.get("Temperature"),
-                values.get("Relative Humidity"),
-                values.get("Dew Point Temperature"),
-                values.get("Wet Bulb"),
-                values.get("Atmospheric Pressure"),
-                values.get("Wind Direction"),
-                values.get("Wind Speed"),
-                values.get("Wind Gust"),
-                values.get("Wind Chill"),
-                values.get("WBGT Index"),
-                values.get("2 Inch Soil"),
-                values.get("4 Inch Soil"),
-                values.get("8 Inch Soil"),
-                values.get("Soil Moisture"),
-                values.get("Solar Radiation"),
-                values.get("Cumulative Rain Since 12:00 AM"),
-                values.get("Max Air Temperature"),
-                values.get("Min Air Temperature"),
-                values.get("Max Wind Speed")
-        );
+        GaStationReading gaStationReading = new GaStationReading(values);
         return gaStationReading;
     }
 
